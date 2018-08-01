cb4e87c4637ff322877d638c7251575969be2daf
Lars Nohle <lars.nohle@gmail.com>
2018-08-01 10:22:04 +0200

    Finished nineteen challenge 1.

---
+++ 19/nineteenChallenge1.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"math"
+)
+
+func getStringInput(msg string, allowedResponses []string) (retString string) {
+	allowedResponsesMap := make(map[string]bool)
+	for _, allowedResponse := range allowedResponses {
+		allowedResponsesMap[allowedResponse] = true
+	}
+
+	return getRestrictedStringInput(msg, allowedResponsesMap)
+}
+
+func getRestrictedStringInput(msg string, allowedResponses map[string]bool) (retString string) {
+	done := false
+	for ; !done; {
+		fmt.Print(msg)
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if len(input) > 0 && allowedResponses[input]{
+			retString = input
+			done = true
+		}
+	}
+	return
+}
+
+func getNonNegativeIntegerInput(msg string) (retInt uint64) {
+	done := false
+	for ; !done; {
+		fmt.Print(msg)
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input = strings.TrimSpace(input)
+		i, err := strconv.ParseUint(input, 10, 64)
+
+		if err == nil {
+			retInt = i
+			done = true
+		}
+	}
+	return
+}
+
+func getHeightAndWeight() (height uint64, weight uint64) {
+	height = getNonNegativeIntegerInput("Height:")
+	weight = getNonNegativeIntegerInput("Weight: ")
+	return
+}
+
+
+func calculateBmiUsingImperialUnits(height uint64, weight uint64) (bmi float64) {
+	bmi = float64(weight) / math.Pow(float64(height), 2) * 703.0
+	return
+}
+
+func calculateBmiUsingMetricUnits(height uint64, weight uint64) (bmi float64) {
+	bmi = float64(weight) / math.Pow(float64(height) / 100.0, 2)
+	return
+}
+
+func printBmiResults(bmi float64) {
+	fmt.Printf("Your BMI is %.1f.\n", bmi)
+	if bmi < 18.5 {
+		fmt.Println("You are underweight")
+	} else if bmi > 25.0 {
+		fmt.Println("You are overweight. You should see your doctor.")
+	} else {
+		fmt.Println("You are within the ideal weight range.")
+	}
+}
+
+
+func main() {
+	metricOrImperial := getStringInput("Metric(m) or imperial(i) units? ", []string{"m", "M", "i", "I"})
+	if metricOrImperial == "i" || metricOrImperial == "I" {
+		printBmiResults(calculateBmiUsingImperialUnits(getHeightAndWeight()))
+	} else {
+		printBmiResults(calculateBmiUsingMetricUnits(getHeightAndWeight()))
+	}
+}
