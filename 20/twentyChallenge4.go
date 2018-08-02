d9c312966c2a23446a2c72a68ddc79e8f3ae63e6
Lars Nohle <lars.nohle@gmail.com>
2018-08-02 07:27:06 +0200

    Finished twenty challenge 4.

---
+++ 20/twentyChallenge4.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+)
+
+const EauClaireCountyExtraTax = 0.005
+const DunnCountyExtraTax = 0.004
+const WisconsinTax = 0.05
+const IllinoisTax = 0.08
+const SwededTax = 0.25
+
+const Illinois = "ILLINOIS"
+const IllinoisAbbreviation = "IL"
+const Wisconsin = "WISCONSIN"
+const WisconsinAbbreviation = "WI"
+const EauClaire = "EAUCLAIRE"
+const Dunn = "DUNN"
+const Sweden = "SWEDEN"
+const SwedenAbbreviation = "SWE"
+
+func getNonNegativeIntegerInput(msg string) (retInt uint64) {
+	done := false
+	for ; !done; {
+		fmt.Print(msg)
+		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		input = strings.TrimSpace(input)
+		i, err := strconv.ParseUint(input, 10, 64)
+
+		if err == nil {
+			retInt = i
+			done = true
+		}
+	}
+	return
+}
+
+// Get non-empty input.
+func getNonEmptyInput(msg string) (input string) {
+	done := false
+	for ; !done; {
+		fmt.Print(msg)
+		input, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		input = strings.TrimSpace(input)
+		if len(input) > 0 {
+			done = true
+		}
+	}
+
+	return input
+}
+
+
+func getInput() (orderAmount uint64, state string, county string) {
+	orderAmount = getNonNegativeIntegerInput("What is the order amount? ")
+	state = strings.ToUpper(getNonEmptyInput("What state do you live in? "))
+	if state == Wisconsin || state == WisconsinAbbreviation {
+		county = strings.ToUpper(getNonEmptyInput("What county do you live in? "))
+	}  else {
+		county = ""
+	}
+
+	return
+}
+
+func calculateTax(orderAmount uint64, state string, county string) (total float64, tax float64) {
+	taxRate := 0.0
+
+	wisconsinTaxMap := make(map[string]float64)
+	wisconsinTaxMap[EauClaire] = EauClaireCountyExtraTax
+	wisconsinTaxMap[Dunn] = DunnCountyExtraTax
+
+	if state == Wisconsin || state == WisconsinAbbreviation {
+		taxRate = WisconsinTax
+		taxRate += wisconsinTaxMap[county]
+	} else if state == Illinois || state == IllinoisAbbreviation {
+		taxRate = IllinoisTax
+	} else if state == Sweden || state == SwedenAbbreviation {
+		taxRate = SwededTax
+	}
+
+	tax = float64(orderAmount) * taxRate
+	total = float64(orderAmount) + tax
+	return
+}
+
+func printOutput(totoal float64, tax float64) {
+	outputMsg := ""
+	if tax > 0.0 {
+		outputMsg = fmt.Sprintf("The tax is $%.2f.\n", tax)
+	}
+
+	outputMsg += fmt.Sprintf("The total is $%.2f.\n", totoal)
+	fmt.Print(outputMsg)
+}
+
+func main() {
+	printOutput(calculateTax(getInput()))
+}
